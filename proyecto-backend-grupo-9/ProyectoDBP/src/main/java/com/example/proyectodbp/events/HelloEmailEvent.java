3e3c770031e214e9d403cf765ca686dc8f8d6b7a
Ggburitox <thiago.rivera@utec.edu.pe>
2024-05-26 15:07:54 -0500

    Eventos Asincronicos

---
+++ proyecto-backend-grupo-9/ProyectoDBP/src/main/java/com/example/proyectodbp/events/HelloEmailEvent.java
@@ -0,0 +1,13 @@
+package com.example.proyectodbp.events;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class HelloEmailEvent extends ApplicationEvent {
+    private final String email;
+    public HelloEmailEvent(String email) {
+        super(email);
+        this.email = email;
+    }
+}
