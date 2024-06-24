292c2b7c429655acfb16c2531232040ed30030a6
Ggburitox <thiago.rivera@utec.edu.pe>
2024-06-24 08:37:10 -0500

    intento de arreglar entidades?

--- proyecto-backend-grupo-9/ProyectoDBP/src/main/java/com/example/proyectodbp/station/domain/Station.java
+++ proyecto-backend-grupo-9/ProyectoDBP/src/main/java/com/example/proyectodbp/station/domain/Station.java
@@ -14,9 +14,7 @@ import java.util.List;
 
 @Entity
 @Data
-@JsonIdentityInfo(
-        generator = ObjectIdGenerators.PropertyGenerator.class,
-        property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Station {
     @Id
     private Long id;
