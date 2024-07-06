726fd29aa34a83e8867729217a66a3700718254b
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2024-07-06 00:18:47 -0500

    Update PassengerRepository.java

--- src/main/java/com/example/proyectodbp/passenger/infraestructure/PassengerRepository.java
+++ src/main/java/com/example/proyectodbp/passenger/infraestructure/PassengerRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
     Optional<Passenger> findByEmail(String email);
+    Optional<Passenger> findByFirstName(String firstName);
 }
