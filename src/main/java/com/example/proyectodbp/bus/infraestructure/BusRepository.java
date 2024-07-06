70336a3d019e7ab4c32a544a884b78b356f95462
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2024-07-06 00:15:31 -0500

    Update BusRepository.java

--- src/main/java/com/example/proyectodbp/bus/infraestructure/BusRepository.java
+++ src/main/java/com/example/proyectodbp/bus/infraestructure/BusRepository.java
@@ -1,10 +1,13 @@
 package com.example.proyectodbp.bus.infraestructure;
 
 import com.example.proyectodbp.bus.domain.Bus;
+import com.example.proyectodbp.station.domain.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BusRepository extends JpaRepository<Bus,Long> {
     Optional<Bus> findByPlate(String plate);
+    List<Bus> findAllByStation(Station station);
 }
