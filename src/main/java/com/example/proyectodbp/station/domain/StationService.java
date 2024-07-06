c7247284d242c892b56b0e726765ccebdc958b36
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2024-07-06 00:22:57 -0500

    Update StationService.java

--- src/main/java/com/example/proyectodbp/station/domain/StationService.java
+++ src/main/java/com/example/proyectodbp/station/domain/StationService.java
@@ -4,6 +4,10 @@ import com.example.proyectodbp.events.HelloEmailEvent;
 import com.example.proyectodbp.exceptions.ResourceNotFoundException;
 import com.example.proyectodbp.exceptions.UnauthorizedOperationException;
 import com.example.proyectodbp.exceptions.UniqueResourceAlreadyExist;
+import com.example.proyectodbp.passenger.domain.Passenger;
+import com.example.proyectodbp.passenger.dto.NewPassengerRequestDto;
+import com.example.proyectodbp.passenger.dto.PassengerDto;
+import com.example.proyectodbp.passenger.infraestructure.PassengerRepository;
 import com.example.proyectodbp.route.domain.Route;
 import com.example.proyectodbp.route.infraestructure.RouteRepository;
 import com.example.proyectodbp.station.dto.NewStationRequestDto;
@@ -19,14 +23,16 @@ public class StationService {
     private final StationRepository stationRepository;
     private final RouteRepository routeRepository;
     private final AuthorizationUtils authorizationUtils;
+    private final PassengerRepository passengerRepository;
     private final ModelMapper modelMapper = new ModelMapper();
     private final ApplicationEventPublisher applicationEventPublisher;
 
-    public StationService(StationRepository stationRepository, RouteRepository routeRepository, AuthorizationUtils authorizationUtils, ApplicationEventPublisher applicationEventPublisher) {
+    public StationService(StationRepository stationRepository, RouteRepository routeRepository, AuthorizationUtils authorizationUtils, ApplicationEventPublisher applicationEventPublisher, PassengerRepository passengerRepository) {
         this.stationRepository = stationRepository;
         this.routeRepository = routeRepository;
         this.authorizationUtils = authorizationUtils;
         this.applicationEventPublisher = applicationEventPublisher;
+        this.passengerRepository = passengerRepository;
     }
 
     public String createStation(NewStationRequestDto newStation) {
@@ -36,15 +42,10 @@ public class StationService {
 
         Station station = modelMapper.map(newStation, Station.class);
         stationRepository.save(station);
-        String message = "Station created";
-        applicationEventPublisher.publishEvent(new HelloEmailEvent(authorizationUtils.getCurrentUserEmail(), message));
         return "/station/" + station.getId();
     }
 
     public StationDto getStationInfo(Long id) {
-        if (!authorizationUtils.isAdminOrResourceOwner(id))
-            throw new UnauthorizedOperationException("User has no permission to modify this resource");
-
         Station station = stationRepository
                 .findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("This station does not exist"));
@@ -53,15 +54,10 @@ public class StationService {
     }
 
     public void deleteStation(Long id) {
-        if (!authorizationUtils.isAdminOrResourceOwner(id))
-            throw new UnauthorizedOperationException("User has no permission to modify this resource");
-
         stationRepository.deleteById(id);
     }
 
     public void updateStation(Long id, StationDto stationDto) {
-        if (!authorizationUtils.isAdminOrResourceOwner(id))
-            throw new UnauthorizedOperationException("User has no permission to modify this resource");
 
         Station stationToUpdate = stationRepository
                 .findById(id)
@@ -69,25 +65,24 @@ public class StationService {
 
         stationToUpdate.setName(stationDto.getName());
         stationRepository.save(stationToUpdate);
-        String message = "Station updated";
-        applicationEventPublisher.publishEvent(new HelloEmailEvent(authorizationUtils.getCurrentUserEmail(), message));
     }
 
-    public void addRoute(Long id, String routeName) {
-        if (!authorizationUtils.isAdminOrResourceOwner(id))
-            throw new UnauthorizedOperationException("User has no permission to modify this resource");
-
+    public void addPassenger(Long id, NewPassengerRequestDto passengerDto) {
         Station station = stationRepository
                 .findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("This station does not exist"));
-        Route route = routeRepository
-                .findByName(routeName)
-                .orElseThrow(() -> new ResourceNotFoundException("This route does not exist"));
-        station.addRoute(route);
-        route.addStation(station);
-        stationRepository.save(station);
-        routeRepository.save(route);
-//        String message = "Route added to station";
-//        applicationEventPublisher.publishEvent(new HelloEmailEvent(authorizationUtils.getCurrentUserEmail(), message));
+
+        Passenger passenger = passengerRepository
+                .findByEmail(passengerDto.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("This passenger does not exist"));
+
+        // Check if the passenger is already in the station's passenger list
+        if (!station.getPassengers().contains(passenger)) {
+            // If the passenger is not in the station's passenger list, add it
+            station.addPassenger(passenger);
+            passenger.setStation(station);
+            stationRepository.save(station);
+            passengerRepository.save(passenger);
+        }
     }
 }
